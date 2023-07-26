9d073918e8dac97912a1cf4827d29f7b1157336d
vinons <vini.onsomu@gamesysgroup.com>
2023-07-26 08:16:24 +0800

    feat/add python beginner code

---
+++ modular/function-02.py
@@ -0,0 +1,5 @@
+def say_hello():
+    print("Hello, friends!")
+
+say_hello()
+say_hello()
---
+++ variables/strings.py
@@ -0,0 +1,17 @@
+cookies = 'Sugar'
+print(cookies)
+print(type(cookies))
+
+cookies = 1
+print(cookies)
+print(type(cookies))
+
+Cookies = 3
+print(Cookies)
+print(cookies)
+
+first_name = "Jadd"
+print(first_name)
+
+first_Name = "Sarah"
+print(first_name)
